df911566386bbb68d3ef87e23f704386024adbc0
Jeff Kimbrel <jakpot@gmail.com>
2023-10-23 19:06:03 -0700

    Update get_mappgene_data.R

--- R/get_mappgene_data.R
+++ R/get_mappgene_data.R
@@ -1,15 +1,13 @@
 #' Load the mappgene data into a tibble
 #'
-#' @param path The path to the folder with the *.ivar.lofreq.snpSIFT.txt files.
-#' @param workflow The mappgene workflow type(s). Accepts a single string, or a vector of both ivar and lofreq.
+#' @param path The path to the folder with the .ivar.snpSIFT.txt and .ivar.lofreq.snpSIFT.txt files.
+#' @param workflow The mappgene workflow type(s). Accepts a single string, or a vector of both "ivar" and "lofreq".
 #' @param sample_cleanup Additional text to remove from the sample names.
 #'
 #' @export
 #'
 
 read_mappgene_data = function(path, workflow = c("ivar", "lofreq"), sample_cleanup = "LLNL_") {
-
-  require(tidyverse)
 
   if (length(setdiff(workflow, c("ivar", "lofreq"))) > 0) {
     stop("Sorry, there are workflow terms I don't recognize. Please use lofreq and/or ivar only")
@@ -24,17 +22,17 @@ read_mappgene_data = function(path, workflow = c("ivar", "lofreq"), sample_clean
     message(paste("Found", length(files), "ivar files"))
 
     ivar = sapply(files,
-                  read_delim,
+                  readr::read_delim,
                   delim = "\t",
                   comment = "##",
                   simplify = FALSE,
-                  col_types = "ficcdifffcciic") %>%
-      lapply(\(x) mutate(x, across(ALT_QUAL, as.double))) %>% # to fix when a file is empty
-      bind_rows(.id = "id")
+                  col_types = "ficcdifffcciic") |>
+      lapply(\(x) dplyr::mutate(x, dplyr::across(ALT_QUAL, as.double))) |> # to fix when a file is empty
+      dplyr::bind_rows(.id = "id")
 
-    ivar = ivar %>%
-      filter(ALT_QUAL > 20) %>%
-      filter(FILTER == 'PASS')
+    ivar = ivar |>
+      dplyr::filter(ALT_QUAL > 20) |>
+      dplyr::filter(FILTER == 'PASS')
 
     l$ivar = ivar
   }
@@ -46,32 +44,32 @@ read_mappgene_data = function(path, workflow = c("ivar", "lofreq"), sample_clean
     message(paste("Found", length(files), "lofreq files"))
 
     lofreq = sapply(files,
-                    read_delim,
+                    readr::read_delim,
                     delim = "\t",
                     comment = "##",
                     simplify = FALSE,
-                    col_types = "ficcdifffcciic") %>%
-      #lapply(\(x) mutate(x, across(ALT_QUAL, as.double))) %>% # to fix when a file is empty
-      bind_rows(.id = "id")
+                    col_types = "ficcdifffcciic") |>
+      #lapply(\(x) mutate(x, across(ALT_QUAL, as.double))) |> # to fix when a file is empty
+      dplyr::bind_rows(.id = "id")
     l$lofreq = lofreq
   }
 
-  df = bind_rows(l, .id = "PIPELINE") %>%
-    mutate(FILE = basename(id)) %>%
-    select(-id) %>%
-    mutate(SAMPLE = FILE) %>%
-    mutate(SAMPLE = str_replace(SAMPLE, ".ivar.snpSIFT.txt", "")) %>%
-    mutate(SAMPLE = str_replace(SAMPLE, ".ivar.lofreq.snpSIFT.txt", "")) %>%
-    mutate(SAMPLE = str_replace(SAMPLE, sample_cleanup, ""))
+  df = dplyr::bind_rows(l, .id = "PIPELINE") |>
+    dplyr::mutate(FILE = basename(id)) |>
+    dplyr::select(-id) |>
+    dplyr::mutate(SAMPLE = FILE) |>
+    dplyr::mutate(SAMPLE = stringr::str_replace(SAMPLE, ".ivar.snpSIFT.txt", "")) |>
+    dplyr::mutate(SAMPLE = stringr::str_replace(SAMPLE, ".ivar.lofreq.snpSIFT.txt", "")) |>
+    dplyr::mutate(SAMPLE = stringr::str_replace(SAMPLE, sample_cleanup, ""))
 
   names(df) = gsub(x = names(df), pattern = "ANN\\[\\*\\]\\.", replacement = "")
 
-  df = df %>%
-    select("SAMPLE", "PIPELINE", "GENE", "FEATUREID", "POS", "REF", "ALT", "AF", "DP", "EFFECT",
-           "HGVS_C", "HGVS_P", "CDNA_POS", "AA_POS", "FILE") %>%
-    unique()%>%
-    mutate(ALT_DP = as.integer(DP * AF)) %>%
-    as_tibble()
+  df = df |>
+    dplyr::select("SAMPLE", "PIPELINE", "GENE", "FEATUREID", "POS", "REF", "ALT", "AF", "DP", "EFFECT",
+           "HGVS_C", "HGVS_P", "CDNA_POS", "AA_POS", "FILE") |>
+    unique()|>
+    dplyr::mutate(ALT_DP = as.integer(DP * AF)) |>
+    tibble::as_tibble()
 
   return(df)
 }
