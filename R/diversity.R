5b889d3e0a38d5a0f79f76c207fc49594aa61392
Jeff Kimbrel <jakpot@gmail.com>
2024-03-15 18:13:42 -0700

    coverage and diversity functions

---
+++ R/diversity.R
@@ -0,0 +1,40 @@
+#' Calculate Hill numbers of order 1, 2, and Inf from a matrix
+#'
+#' @param x a matrix with nrows samples and ncol counts
+#'
+#' @return a data.frame with Hill's N_1, N_2, N_Inf for each sample
+#' @details log(N_1) is entropy, log(N_2) is inverse Simpson's
+
+hill_12I <- function(x) {
+  vegan::renyi(x, scales = c(1, 2, Inf), hill = TRUE)
+}
+
+#' Calculate the Simpson index for a set of frequencies
+#'
+#' @param x a numeric vector
+#'
+#' @return the Simpson index
+#'
+#' @details The Simpson index is the probability that two bases are the
+#'          same. Aka the reciprocal of N_2. Aka π for 1 nt.
+#' @seealso vegan::diversity
+
+simpson <- function(x) {
+  1 / hill_12I(x)["2"]
+}
+
+
+
+
+#' Calculate the Gini-Simpson index for a set of frequencies
+#'
+#' @param x a numeric vector
+#'
+#' @return the Gini-Simpson index
+#'
+#' @details The Gini-Simpson is 1 minus the probability that two bases are the
+#'          same. Aka 1 minus Simpson's index. Aka 1 minus 1/N_2
+#' @seealso vegan::diversity
+gini_simpson <- function(x) {
+  1 - simpson(x)
+}
---
+++ R/get_coverages.R
@@ -0,0 +1,54 @@
+#' Make coverage files from folder with bedgraphs
+#'
+#' @param file_path A directory with bedgraph files
+#'
+#' @export
+
+
+
+get_coverages = function(file_path) {
+  bed_list = list.files(path = file_path, pattern = "*ivar.bedgraph", full.names = T, recursive = TRUE)
+
+  coverage = sapply(bed_list, readr::read_delim, col_names = c("C", "START", "STOP", "COV"),  delim = "\t", simplify=FALSE) |>
+    dplyr::bind_rows(.id = "SAMPLE") |>
+    dplyr::mutate(FILE = dirname(SAMPLE)) |>
+    dplyr::mutate(FILE = stringr::str_remove(FILE, file_path)) |>
+    tidyr::separate(FILE, into = c("FILE"), sep = "/") |>
+    dplyr::mutate(SAMPLE = basename(SAMPLE)) |>
+    dplyr::mutate(SAMPLE = stringr::str_remove(SAMPLE, ".ivar.bedgraph")) |>
+    dplyr::mutate(POS = purrr::map2(START, STOP - 1, ~ seq(from = .x, to = .y))) |>
+    dplyr::select(SAMPLE, COV, POS) |>
+    tidyr::unnest(cols = c(POS)) |>
+    dplyr::arrange(POS)
+
+  coverage
+}
+
+
+
+#' Plot ECDF coverage
+#'
+#' @param coverage A coverage from dataframe
+#' @param pseudocount A pseudocount to add to coverage to make log10 transformation work
+#' @param intercept An x value to draw a vertical line
+#'
+#' @export
+
+plot_coverage_ecdf = function(coverage, pseudocount = 1, intercept = 100) {
+  coverage |>
+    dplyr::mutate(COV = COV + pseudocount) |>
+    ggplot2::ggplot(ggplot2::aes(x = COV)) +
+    ggplot2::stat_ecdf(pad = F) +
+    ggplot2::theme(legend.position = "bottom") +
+    ggplot2::geom_vline(xintercept = intercept, linetype = 2, color = "#ff0000aa", linewidth = 1) +
+    ggplot2::facet_wrap(~SAMPLE) +
+    ggplot2::scale_x_continuous(labels=function(x) format(x,
+                                                 big.mark = ",",
+                                                 decimal.mark = ".",
+                                                 scientific = FALSE),
+                       trans = "log10") +
+    ggplot2::labs(x = "Coverage (log10)",
+         y = "Cumulative Distribution")
+}
+
+
